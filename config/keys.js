13954e836ff5d1df22b9e82ef854bd362276c1d6
Dharmendra Sarswat <dharmasarswat001@gmail.com>
2021-08-08 15:50:32 +0530

    changed mongodb pass.

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-dbPassword = 'mongodb+srv://test:'+ encodeURIComponent('*****') + '@lottery-pwzb7.mongodb.net/users?retryWrites=true&w=majority';
-
-session_secrete = 'jguewibwfvsufeftwwsbvugf',
-
 module.exports = {
-    mongoURI: dbPassword,
-    session_secrete
+	mongoURI:
+		"mongodb+srv://test:<password>@lottery-pwzb7.mongodb.net/binaryVedas?retryWrites=true&w=majority",
+	session_secrete: "jguewibwfvsufeftwwsbvugf",
 };
